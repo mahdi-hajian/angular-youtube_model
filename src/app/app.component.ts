6de34af4f7a34f03aa7cda21f7e137dc69566aa5
mahdi hajian <mahdi.haj.1375@gmail.com>
2018-06-06 13:16:37 +0430

    add project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  async ngAfterViewInit() {
+		await this.loadScript('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js');
+		await this.loadScript('https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js');
+	}
+private loadScript(scriptUrl: string) {
+	   return new Promise((resolve, reject) => {
+		 const scriptElement = document.createElement('script');
+		 scriptElement.src = scriptUrl;
+		 scriptElement.onload = resolve;
+		 document.body.appendChild(scriptElement);
+		})
+   }
+
   title = 'app';
 }
+
+
+
+// "./src/assets/fontawesome-free-5.0.12/web-fonts-with-css/css/fa-brands.min.css",
+// "./src/assets/fontawesome-free-5.0.12/web-fonts-with-css/css/fa-regular.min.css",
+// "./src/assets/fontawesome-free-5.0.12/web-fonts-with-css/css/fa-solid.min.css",
+// "./src/assets/fontawesome-free-5.0.12/web-fonts-with-css/css/fontawesome-all.min.css",
+// "./src/assets/fontawesome-free-5.0.12/web-fonts-with-css/css/fontawesome.min.css"
