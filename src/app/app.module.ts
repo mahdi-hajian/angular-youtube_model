c79fef6568506ee39a8579098a730a3ac9c1aeaa
mahdi hajian <mahdi.haj.1375@gmail.com>
2018-06-29 23:00:03 +0430

    ready to next branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,49 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { StarComponent } from './star/star.component';
-import { OneComponenetComponent } from './Servics-Component/one-componenet/one-componenet.component';
-import { TwoComponenetComponent } from './Servics-Component/two-componenet/two-componenet.component';
-import { AccountService } from './Services/account/account.service';
-import { AddAccountService } from './Services/add-account/add-account.service';
 import { AppRoutingModule } from './/app-routing.module';
-import { AdminPanelComponent } from './AdminPanel/AdminPanel.component';
-import { LogInService } from './Services/Guard/logIn.service';
-import { AuthGuardService } from './Services/Guard/AuthGuardService';
-import { UserComponent } from './AdminPanel/User/User.component';
-import { EditUserComponent } from './AdminPanel/editUser/editUser.component';
-import { UserService } from './Services/account/user.service';
 import { NotFounPageComponent } from './not-foun-page/not-foun-page.component';
-import { CanDeactivateGuardService } from './Services/Guard/canDeactivateGuard.service';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      HomeComponent,
-      AboutComponent,
-      StarComponent,
-      OneComponenetComponent,
-      TwoComponenetComponent,
-      AdminPanelComponent,
-      UserComponent,
-      EditUserComponent,
-      NotFounPageComponent
+      NotFounPageComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       FormsModule
    ],
-   providers: [
-      AccountService,
-      AddAccountService,
-      LogInService,
-      AuthGuardService,
-      UserService,
-      CanDeactivateGuardService
-   ],
+   providers: [],
    bootstrap: [
       AppComponent
    ]
