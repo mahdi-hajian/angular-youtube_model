eda5d952737c130384785523bb771903fa1b0777
mahdi hajian <mahdi.haj.1375@gmail.com>
2018-07-04 17:37:50 +0430

    edit

--- src/app/Components/sing-up-reactive/sing-up-reactive.component.ts
+++ src/app/Components/sing-up-reactive/sing-up-reactive.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { resolve } from 'q';
 // add ReactiveFormsModule in app.module
 
 @Component({
@@ -10,6 +12,7 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class SingUpReactiveComponent implements OnInit {
   genders =['', 'male', 'female'];
   inputForm: FormGroup;
+  bannedEmail=['mahdi@mahdi', 'mohammad@mo']
 
   constructor() { }
 
@@ -17,17 +20,39 @@ export class SingUpReactiveComponent implements OnInit {
     this.inputForm = new FormGroup({
       'info': new FormGroup({
         'username': new FormControl(null,Validators.required),
-        'email': new FormControl(null,[Validators.required,Validators.email])
+        'email': new FormControl(null,[Validators.required,Validators.email,this.bannedEmailCheck.bind(this)])
       }),
       'gender': new FormControl('male',Validators.required),
       'colors': new FormArray([])
     });
+    //
+
   }
   onSubmit(){
-    console.log(this.inputForm.value);
+    console.log(this.inputForm);
   }
   onAddColorsClick(){
-    
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.inputForm.get('colors')).push(control);
+  }
+  bannedEmailCheck(control: FormControl): {[e: string]: boolean}{
+    // if find him
+    if (this.bannedEmail.indexOf(control.value) != -1) {
+      return {'isBannedEmail':true}
+    }
+      return null;
   }
 
+  bannedUserName(control: FormControl): Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject)) => {
+      setTimeout(() => {
+        if (control.value == 'mahdi') {
+          resolve({'usenameISBan': true})
+        }else {
+          resolve (null);
+        }
+      }, 2000);
+    }
+    return promise
+  }
 }
